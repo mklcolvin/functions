84a3fe8d694feef4b2c088c95f4c111573d04843
mklcolvin <mklcolvin@gmail.com>
2025-08-13 19:29:01 -0500

    Final version of functions lesson

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let x = plus_one(5);
 
-    another_function(5);
+    println!("The value of x is: {x}");
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn plus_one(x: i32) -> i32 {
+    x + 1;
 }
